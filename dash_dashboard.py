1f96e61616b712a7ee80c75f6cc5f8b106e1bdb9
maiiam <shixuemai@outlook.com>
2023-02-27 17:49:30 -0500

    formatting

--- dash_dashboard.py
+++ dash_dashboard.py
@@ -1,16 +1,33 @@
-from dash import Dash, html, dcc
+import pandas as pd
+import numpy as np
+from dash import Dash, html, dcc, Input, Output, dash_table, callback, State
 import dash_bootstrap_components as dbc
-import plotly.express as px
-import pandas as px
 from fred_data import FredData
 
-app = Dash(
-    external_stylesheets=[dbc.themes.FLATLY]
+app = Dash(__name__, external_stylesheets=['/assets/style.css'])
+
+app.layout = html.Div(
+    children=[
+        html.H1('Economic Tracker', className='text'),
+        html.H4(id='Name-Of-Graph', className='text'),
+        dcc.Graph(id='Graph'),
+        dcc.Interval(id='interval-component', interval=10*1000, n_intervals=0)
+    ],
+    className='background'
 )
 
-app.layout = dbc.Alert(
-    "Hello, Bootstrap!", className="m-5"
-)
 
-if __name__ == "__main__":
-    app.run_server()
+@app.callback(
+    [Output('Name-Of-Graph', 'children'), Output('Graph', 'figure')],
+    [Input('interval-component', 'n_intervals')]
+)
+def update_graph(n):
+    fred_instance = FredData()
+    fred_instance.get_data(name='S&P500', series_id='SP500')
+    name_of_graph = fred_instance.name
+    fig = fred_instance.plot_data()
+    return name_of_graph, fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- fred_data.py
+++ fred_data.py
@@ -6,23 +6,24 @@ import plotly.express as px
 
 data_source='fred'
 default_end_date=date.today()
-default_start_date=default_end_date - datetime.timedelta(days=365*10)
+default_start_date=default_end_date - datetime.timedelta(days=365*5)
 # default_start_date_str=datetime.datetime.strftime(default_start_date,"%Y-%m-%d")
 # default_end_date_str=datetime.datetime.strftime(default_end_date,"%Y-%m-%d")
 
 
 class FredData:
-    def __init__(self, name='S&P500',data_source='fred',series_id='SP500',start_date=default_start_date,end_date=default_end_date):
-        self.data_source = data_source
+    def __init__(self):
+        self.data_source = 'fred'
+    
+    def get_data(self,name,series_id,start_date=default_start_date,end_date=default_end_date):
+        self.name=name
         self.series_id = series_id
         self.start_date = start_date
         self.end_date = end_date
-        self.name= name
-        self.data=pd.DataFrame()
-    
-    def get_data_and_plot(self):
         self.data=web.DataReader(self.series_id,self.data_source,self.start_date,self.end_date).reset_index().dropna()
         self.data=self.data.rename(columns={"DATE":'Date',self.series_id:"Value"})
+    
+    def plot_data(self):
         fig = px.line(self.data,x='Date',y='Value')
         
         fig.update_layout(
@@ -41,7 +42,8 @@ class FredData:
         return fig
         
 if __name__ == '__main__':
-    fred=FredData(start_date='2020-01-01')
-    fred.get_data()
-    fred.plot_data()
+    fred=FredData()
+    fred.get_data(name='S&P500',series_id='SP500',start_date='2020-01-01')
+    f=fred.plot_data()
+    f.show()
     
